debffa85eed72acd9c5716d9558e5ce2d907315e
pyounani <yndbsk9372@gmail.com>
2024-11-16 22:57:31 +0900

    [Feat] AOP 실전 에제

---
+++ src/test/java/pyounani/aop/exam/ExamTest.java
@@ -0,0 +1,26 @@
+package pyounani.aop.exam;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import pyounani.aop.exam.aop.RetryAspect;
+import pyounani.aop.exam.aop.TraceAspect;
+
+@Slf4j
+@Import({TraceAspect.class, RetryAspect.class})
+@SpringBootTest
+public class ExamTest {
+
+    @Autowired
+    ExamService examService;
+
+    @Test
+    void test() {
+        for (int i = 0; i < 5; i++) {
+            log.info("client request i={}", i);
+            examService.request("data" + i);
+        }
+    }
+}
